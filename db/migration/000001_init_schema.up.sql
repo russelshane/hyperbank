92c7cba2ed00c81112a5e10c4fa0f6db76767aab
dars <darsler@pm.me>
2022-09-26 20:20:08 +0800

    chore: update init sql schema

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -38,5 +38,4 @@ CREATE INDEX ON "transfers" ("to_account_id");
 CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
 
 COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
-
 COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
